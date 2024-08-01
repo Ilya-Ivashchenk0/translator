6b869ec0172205db90973a5e7e953536b80e0a76
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-08-01 10:23:18 +0300

    fix: pritter

--- src/shared/ui/post/post.tsx
+++ src/shared/ui/post/post.tsx
@@ -19,13 +19,13 @@ export const Post: React.FC<postProps> = ({
       onClick={() => router.push(`/ru/post/${id}`)}
       className="cursor-pointer bg-secondary-bg-color rounded relative overflow-hidden flex flex-col justify-center items-center px-4 pt-4 pb-20 gap-4"
     >
-      <div className='w-full flex flex-col gap-4'>
-      <h3 className="rounded-xl bg-primary-bg-color w-max px-4 text-secondary-text-color text-xl">
-        {title}
-      </h3>
-      <p className="text-lg text-secondary-text-color line-clamp-3 max-h-40">
-        {text}
-      </p>
+      <div className="w-full flex flex-col gap-4">
+        <h3 className="rounded-xl bg-primary-bg-color w-max px-4 text-secondary-text-color text-xl">
+          {title}
+        </h3>
+        <p className="text-lg text-secondary-text-color line-clamp-3 max-h-40">
+          {text}
+        </p>
       </div>
       <small className="absolute bottom-4 left-4 text-primary-text-color text-xs">{`${date}`}</small>
       <div className="absolute bottom-4 right-4 flex items-end gap-4">
